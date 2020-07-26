e366da8fbd063bac82b62fa29d4bd8b2f19d61ba
Koten <koten0224@gmail.com>
2020-07-26 21:30:38 +0800

    define flexible column function

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,26 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  
+  def self.flexible_column(*input_columns, **options)
+    unless instance_methods.include? column_name
+      has_many :columns, class_name: "Attribute", as: :object
+    end
+    default_value = options[:default]
+    data_type = options[:type]
+    
+    input_columns.each do |column_name|      
+
+      define_method(column_name) do
+        value = columns.find_or_create_by(name: column_name) do |column|
+          column.value = default_value
+        end.value
+        value.to_i if data_type == :integer
+      end
+
+      define_method(column_name.to_s + '=') do |value|
+        columns.find_or_create_by(name: column_name).update(value: value)
+      end
+
+    end
+  end
 end
